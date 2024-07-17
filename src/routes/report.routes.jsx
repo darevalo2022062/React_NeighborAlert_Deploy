420932da377e1b6392ddecd7ca3215503cd6bbe4
Jonathan <jdomingo-2022206@kinal.edu.gt>
2024-07-17 14:22:35 -0600

    routesProtection conf a little bit

--- src/routes/report.routes.jsx
+++ src/routes/report.routes.jsx
@@ -1,9 +1,11 @@
 import React, { lazy } from 'react';
+import PrivateRoute from './PrivateRoutes.jsx';
 
 const Reports = lazy(() => import('../pages/Reports'));
 
+
 const reportRoutes = [
-    { path: '/reports', element: <Reports /> },
+    { path: '/reports', element: <PrivateRoute element={<Reports />} /> },
 ]
 
 export default reportRoutes;
